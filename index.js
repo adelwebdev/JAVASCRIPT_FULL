e5f57d00ce60f4a5cf29c92d0dedb26c6ab150e4
Adel <83227627+adelwebdev@users.noreply.github.com>
2021-07-31 19:41:18 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const btn = document.querySelector("#btn");
-const img2 = document.getElementById("img");
-
+const photo = document.getElementById("img");
 console.log(btn);
 console.log(img);
 
 btn.addEventListener("click", () => {
-  //   console.log("je click!");
-  //   // pour ajouter une classe!
-  img2.classList.toggle("show");
+  console.log("je click!");
+  // pour ajouter une classe!
+  photo.classList.toggle("montre");
   alert("ca change");
 });
 
@@ -17,4 +16,26 @@ const vertical = document.querySelector(".vertical");
 
 mouseEvent.addEventListener("mousemove", (e) => {
   console.log(e);
+  //on recupère les valeurs de x et y
+  horizontal.innerHTML = e.x;
+  vertical.innerHTML = e.y;
+  mouseEvent.style.left = (e.x / window.innerWidth) * 100 + "%";
+  //   mouseEvent.style.top = (e.y / window.innerHeight) * 100 + "%";
+
+  if (e.x > 500) {
+    document.body.style.filter = "blur(3px)";
+  } else {
+    document.body.style.filter = "none";
+  }
+  if (e.y > 150) {
+    mouseEvent.style.background = "skyblue";
+  } else {
+    mouseEvent.style.background = "none";
+  }
+});
+
+document.getElementById("input").addEventListener("input", (e) => {
+  // pour récuperer les valeus des input
+  console.log(e.target.value);
+  vertical.innerHTML = e.target.value;
 });
