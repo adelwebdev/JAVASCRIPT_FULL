f3dc4ed38ce981680fa09f58c5e6eb82c56e030d
Adel <83227627+adelwebdev@users.noreply.github.com>
2021-08-06 14:58:31 +0300

    Add files via upload

--- DOM_JS/cours/script.js
+++ DOM_JS/cours/script.js
@@ -10,7 +10,6 @@ baliseHtml.style.background = "skyblue";
 
 // les Events ********
 //tjrs loger les variables ********
-
 const questionContainer = document.querySelector(".click-event");
 const btn1 = document.querySelector("#btn-1");
 const btn2 = document.getElementById("btn-2");
@@ -40,4 +39,56 @@ btn2.addEventListener("click", () => {
   //   console.log("je click!");
   response.classList.add("showResponse");
   response.style.background = "red";
+});
+//-------------------------------------------------------------------
+// -----  Mouse Events -------
+const mousemove = document.querySelector(".mousemove");
+// console.log(mousemove);
+window.addEventListener("mousemove", (e) => {
+  //   console.log(e);
+  //   ATTENTION!!! faut concatener le +px!!
+  mousemove.style.left = e.pageX + "px";
+  mousemove.style.top = e.pageY + "px";
+});
+
+window.addEventListener("mousedown", () => {
+  // il faut refaire le translate car il a été écrasé par le nouveau style injecté
+  mousemove.style.transform = "scale(2) translate(-25%, -25%)";
+  //   console.log("test");
+});
+
+window.addEventListener("mouseup", () => {
+  mousemove.style.transform = "scale(1) translate(-50%, -50%)";
+  //   console.log("test");
+  mousemove.style.border = "2px solid brown";
+});
+
+questionContainer.addEventListener(
+  "mouseenter",
+  () => (questionContainer.style.background = "rgba(0,0,0,0.6)")
+);
+questionContainer.addEventListener("mouseout", () => {
+  //   console.log("on est out");
+  questionContainer.style.background = "brown";
+});
+response.addEventListener("mouseover", () => {
+  response.style.transform = "rotate(2deg)";
+});
+
+// ----------------------------le KEY PRESS -------------------------
+const keypress = document.querySelector(".keypress");
+const key = document.getElementById("key");
+console.log(keypress);
+console.log(key);
+document.addEventListener("keypress", (e) => {
+  console.log(e.key);
+  //   key.innerHTML = e.key;
+  key.textContent = e.key;
+  if (e.key === "a") {
+    keypress.style.background = "orange";
+  } else if (e.key === "z") {
+    keypress.style.background = "violet";
+  } else {
+    keypress.style.background = "grey";
+  }
 });
