05cefb480832829cb7a15e0dccd14b39dbeabe91
Adel <83227627+adelwebdev@users.noreply.github.com>
2021-08-08 14:11:49 +0300

    Add files via upload

--- DOM_JS/calculator/index.js
+++ DOM_JS/calculator/index.js
@@ -1,1 +1,25 @@
-alert('"ca marche');
+const buttons = document.querySelectorAll(".btn");
+console.log(buttons);
+
+const result = document.getElementById("result");
+console.log(result);
+// somme = "";
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    console.log(e.target.id);
+    // somme = e.target.id;
+    // result.innerHTML = `<h3>${somme}</h3>`;
+    result.textContent += e.target.id;
+  });
+});
+
+equal.addEventListener("click", () => {
+  // la méthode EVAL! pr que JS calcul le contenu du texte (comme une var Number)
+  result.textContent = eval(result.textContent);
+});
+
+clear.addEventListener("click", () => {
+  //pour vider le contenu de la variable: result => result.textContent = ""
+  result.textContent = "";
+});
