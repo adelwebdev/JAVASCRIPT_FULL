4b2cac91b09b7a327346c857a3e981a248de19e1
Adel <83227627+adelwebdev@users.noreply.github.com>
2021-08-09 22:36:57 +0300

    Add files via upload

--- DOM_JS/bubble-js/index.js
+++ DOM_JS/bubble-js/index.js
@@ -1,10 +1,45 @@
-const bubble = document.createElement("span");
-bubble.classList.add("bubble");
-document.body.appendChild(bubble);
+// Creer des bulles
+//********************** */
 
-const size = Math.random() * 200 + 100 + "px";
-bubble.style.height = size;
-bubble.style.width = size;
+const counter = document.querySelector("h3");
+console.log(counter);
+let count = 0;
 
-bubble.style.top = Math.random() * 100 + 50 + "%";
-bubble.style.left = Math.random() * 100 + "%";
+const bubbleMaker = () => {
+  // creation d'une balise Span sur le dom
+  const bubble = document.createElement("span");
+  // insertion de la classe .bubble (crée en css)
+  bubble.classList.add("bubble");
+  //
+  document.body.appendChild(bubble);
+
+  //pour des tailles aléatoires avec méthode Random
+  const size = Math.random() * 200 + 100 + "px";
+  bubble.style.height = size;
+  bubble.style.width = size;
+
+  //pour les positions des bulles (avec Top et Left) et random pour position aléatoire
+  bubble.style.top = Math.random() * 100 + 50 + "%";
+  bubble.style.left = Math.random() * 100 + "%";
+
+  // on crée plusMinus que les bulles aillent plus vers la gauche, (--left négatif)
+  const plusMinus = Math.random() > 0.5 ? 1 : -1;
+  //pour toucher la variable Left (creé en css) et lui injecter Math.random (pr fixer où la bulle ira; voir css)
+  bubble.style.setProperty("--left", Math.random() * 100 * plusMinus + "%");
+
+  // pour suprimer une bulles du DOM et comptabiliser dans "count": nbr de suppression et inserer dans counter avec textContent
+  bubble.addEventListener("click", () => {
+    count++;
+    bubble.remove();
+    console.log(count);
+    counter.textContent = count;
+  });
+
+  // pour fixer un temps de fin aux bulles
+  setTimeout(() => {
+    bubble.remove();
+  }, 8000);
+};
+
+// pour creer plusieurs bulles!! avec: setInterval
+setInterval(bubbleMaker, 300);
